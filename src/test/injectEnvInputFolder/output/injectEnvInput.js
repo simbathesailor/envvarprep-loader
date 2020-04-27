511b43ce211a3b1c70ef3b9e261d6cab934a6ad7
simbathesailor <anilchaudhary453@gmail.com>
2020-04-27 22:06:23 +0530

    fix

--- src/test/injectEnvInputFolder/output/injectEnvInput.js
+++ src/test/injectEnvInputFolder/output/injectEnvInput.js
@@ -5,13 +5,16 @@ console.log('SOMEPARAM outside', SOMEPARAM);
 function App() {
   console.log(process.env.NODE_ENV);
   console.log('SOMEPARAM inside', SOMEPARAM);
-  console.log(`I am param three`);
+  console.log(
+    `I am param three`
+  );
   return React.createElement(
     'div',
     {
       className: 'App',
     },
-    `I am param three` === 'I AM PARAM THREE' &&
+    `I am param three` ===
+      'I AM PARAM THREE' &&
       React.createElement('p', null, 'Conditional got rendered')
   );
 }
