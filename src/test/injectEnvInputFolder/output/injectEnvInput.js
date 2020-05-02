cdbecd041c2c4d2988ee3a2c1206858b7e4a838e
simbathesailor <anilchaudhary453@gmail.com>
2020-05-02 17:20:28 +0530

    add more jest timeout

--- src/test/injectEnvInputFolder/output/injectEnvInput.js
+++ src/test/injectEnvInputFolder/output/injectEnvInput.js
@@ -5,10 +5,15 @@ console.log('SOMEPARAM outside', SOMEPARAM);
 function App() {
   console.log(process.env.NODE_ENV);
   console.log('SOMEPARAM inside', SOMEPARAM);
-  console.log("I am param three");
-  return React.createElement('div', {
-    className: 'App'
-  }, "I am param three" === 'I AM PARAM THREE' && React.createElement('p', null, 'Conditional got rendered'));
+  console.log('I am param three');
+  return React.createElement(
+    'div',
+    {
+      className: 'App',
+    },
+    'I am param three' === 'I AM PARAM THREE' &&
+      React.createElement('p', null, 'Conditional got rendered')
+  );
 }
 
 export default App;
